e7bd773bf88e0b4e1e09d520cb29e46ca3f25a64
Andreza Feu <andrezafeu@gmail.com>
2018-05-07 22:33:46 -0400

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403000302) do
+ActiveRecord::Schema.define(version: 20180508012152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
