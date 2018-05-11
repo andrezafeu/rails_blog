4d3dbe68e4583b9fdff50b78f65f4877b8ffbd23
Andreza Feu <andrezafeu@gmail.com>
2018-05-11 13:29:57 -0400

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
   resources :pages
 
   resources :posts do
-    get    '/comments',          to: 'comments#index', as: 'comments'
-    post   '/comments',          to: 'comments#create'
-    get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
-    get    '/comments/:id',      to: 'comments#show',  as: 'comment'
-    get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
-    patch  '/comments/:id',      to: 'comments#update'
-    delete '/comments/:id',      to: 'comments#destroy'
+    resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
