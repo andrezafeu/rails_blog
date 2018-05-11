36955a869daa85d5e7784c6adf8144abdcb6e02e
Andreza Feu <andrezafeu@gmail.com>
2018-05-11 13:26:15 -0400

    Nest comment routes under posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
   resources :pages
 
-  get    '/posts/:post_id/comments',          to: 'comments#index', as: 'post_comments'
-  post   '/posts/:post_id/comments',          to: 'comments#create'
-  get    '/posts/:post_id/comments/new',      to: 'comments#new',   as: 'new_post_comment'
-  get    '/posts/:post_id/comments/:id',      to: 'comments#show',  as: 'post_comment'
-  get    '/posts/:post_id/comments/:id/edit', to: 'comments#edit',  as: 'edit_post_comment'
-  patch  '/posts/:post_id/comments/:id',      to: 'comments#update'
-  delete '/posts/:post_id/comments/:id',      to: 'comments#destroy'
+  resources :posts do
+    get    '/comments',          to: 'comments#index', as: 'comments'
+    post   '/comments',          to: 'comments#create'
+    get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+    get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+    get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+    patch  '/comments/:id',      to: 'comments#update'
+    delete '/comments/:id',      to: 'comments#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
