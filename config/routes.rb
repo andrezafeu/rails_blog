e15cd20219cd6b2cb92b80181846d88ec038bd73
Andreza Feu <andrezafeu@gmail.com>
2018-05-11 13:18:19 -0400

    Add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :posts
   resources :pages
+
+  get    '/posts/:post_id/comments',          to: 'comments#index', as: 'post_comments'
+  post   '/posts/:post_id/comments',          to: 'comments#create'
+  get    '/posts/:post_id/comments/new',      to: 'comments#new',   as: 'new_post_comment'
+  get    '/posts/:post_id/comments/:id',      to: 'comments#show',  as: 'post_comment'
+  get    '/posts/:post_id/comments/:id/edit', to: 'comments#edit',  as: 'edit_post_comment'
+  patch  '/posts/:post_id/comments/:id',      to: 'comments#update'
+  delete '/posts/:post_id/comments/:id',      to: 'comments#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
