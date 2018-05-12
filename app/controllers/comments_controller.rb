83572f2dd0f5a4fd3c819953a1e783f0f93200d5
Andreza Feu <andrezafeu@gmail.com>
2018-05-12 16:07:57 -0400

    Add action to udpate comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,26 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to @post
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to @post
+  end
+
   private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:name, :content)
   end
